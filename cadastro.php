cd87204e5cb78dcade02e2b4d7cb0b76dd5f1f41
flaviaglenda <flaviaglenda15@gmail.com>
2025-01-28 11:58:01 -0300

    cadastro

--- cadastro.php
+++ cadastro.php
@@ -1,4 +1,24 @@
 <?php
+include 'conexao.php';
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (isset($_POST['email']) && isset($_POST['senha']) && isset($_POST['nome'])) {
+        $nome_usuario = $_POST['nome'];
+        $email_usuario = $_POST['email'];
+        $senha_usuario = password_hash($_POST['senha'], null);
+
+        $query = "INSERT INTO usuarios (email_usuario, senha_usuario, nome_usuario) VALUES (?, ?, ?)";
+        $statement = $conexao->prepare($query);
+        $statement->bind_param("sss", $email_usuario,$senha_usuario, $nome_usuario);
+        $statement->execute();
+        echo "usuario registrado";
+}
+
+        if ($conexao->error) {
+            die("Erro: {$conexao->error}");
+}
+}
 ?>
 
 <!DOCTYPE html>
@@ -6,24 +26,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
     <form action="" method="post">
         <h1>Realize seu cadastro!</h1>
 
-        <label for="nome">Nome:</label>
+        <label for="">Nome:</label>
         <input type="text" id="nome" name="nome" required>
 
-        <label for="email">Email:</label>
+        <label for="">Email:</label>
         <input type="email" id="email" name="email" required>
 
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required>
+        <label for="">Senha:</label>
+        <input type="password" name="senha" required>
 
         <input type="submit" value="Cadastrar">
-        <a href="login.php">Já possui cadastro? Faça login!</a>
-        <a href="esqueci_senha.php">Esqueci minha senha!</a>
+        <a href="index.php">Já possui cadastro? Faça login!</a>
     </form>
 </body>
 </html>
